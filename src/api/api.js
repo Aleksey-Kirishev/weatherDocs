b9dc88e86f0f9c8c1ca0c25c589bea5d6cd84c58
Aleksey-Kirishev <73190160+Aleksey-Kirishev@users.noreply.github.com>
2021-03-10 21:05:48 +0300

    hello

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ const ApiKey = "eec2ffbbadb66ea910b3f122d22f5700"
 
 export const weatherDataAPI = {
     getWeatherData (city) {
-        return fetch(`http://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=${ApiKey}` + `&lang=ru`)
+        return fetch(`https://api.openweathermap.org/data/2.5/weather?q=` + city + `&appid=${ApiKey}` + `&lang=ru`)
             .then(response => response.json()).then(data => data)
     }
 }
